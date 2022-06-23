cb58c513dffe5f2b5b7b1301757f8e93b371bb44
Chamath Perera <chamathpererab@gmail.com>
2022-06-24 00:30:01 +0100

    Update

--- Webscraping_fn.py
+++ Webscraping_fn.py
@@ -7,7 +7,6 @@ def webscrape_DRG(*code):
     import requests
     from bs4 import BeautifulSoup
     import re
-    #import webbrowser
 
     #Set max table column width and allow text wrapping. 
     pd.set_option('display.max_colwidth', 0)
@@ -32,30 +31,33 @@ def webscrape_DRG(*code):
 
     #Print the table in the output
     df
-
+    
+    #Create empty lists that will hold data from webscraped DRG codes
     code = []
     FCP = []
     FOP = []
 
-    #Webscraping commences 
+    #Web scraping using a for loop that passes through all URLs
     for url in urls[:]:
         page = requests.get(url).text
         soup = BeautifulSoup(page, 'html.parser')
         name = soup.find('blockquote')
         name.br.extract()
         code.append(name.text[1:-213])
-        FOP.append(soup.find(id="drg_t_op_pmt").getText())
-        FCP.append(soup.find(id="drg_t_cp_pmt").getText())
+        FOP.append(soup.find(id="drg_t_op_pmt").getText()) #id in the DRG website for the Federal Operating Payment
+        FCP.append(soup.find(id="drg_t_cp_pmt").getText()) #id in the DRG website for the Federal Capital Payment
 
     FCP = [item.strip() for item in FCP]
     FOP = [item.strip() for item in FOP]
 
+    #Column names for the df table 
     df["DRG Codes"] = code
     df["Federal Capital Payment"] = FCP
     df["Federal Operating Payment"] = FOP
 
     intFCP = []
 
+    #Extracting only the numbers from the extracted data. 
     for element in FCP:
         x= re.findall(r"\$[^\]]+", element)
         x = ''.join(x)
@@ -70,16 +72,20 @@ def webscrape_DRG(*code):
         x= x.replace("$","").replace(",","")
         intFOP.append(float(x))
         
-
+    #Sum of the two numbers FOP + FCP
     sum_list = [a + b for a, b in zip(intFCP, intFOP)]
 
+    #Empty list for the Total Federal Payment
     TFP = []
 
+    #Apply the $ formatting to the number
     for element in sum_list:
         TFP.append("${:,.2f}".format(element))
-        
+    
+    #Column name     
     df["Total Federal Payment"] = TFP
 
+    #Return the data table
     return(df)
 
 #%%
